593c23e5db8a23d2e9f74363c45e1837311bc2d9
Prem Keerthi <pkeerthi@Prems-Air.lan>
2024-03-22 17:23:00 -0500

    pushing the latest components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,9 +45,9 @@ const NavBar =() => {
           </Nav>
           <span className='navbar-text'>
             <div className='social-icon'> 
-                <a href="#"> <img src={navIcon1}  alt=""/> </a>
-                <a href="#"> <img src={navIcon2}  alt=""/> </a>
-                <a href="#"> <img src={navIcon3}  alt=""/> </a>
+                <a href="https://www.linkedin.com/in/pkeerthi52/" target= "_blank"> <img src={navIcon1}  alt=""/> </a>
+                <a href="https://www.facebook.com/premkalyan234" target= "_blank"> <img src={navIcon2}  alt=""/> </a>
+                <a href="https://www.instagram.com/premkalyan_k/" target= "_blank"> <img src={navIcon3}  alt=""/> </a>
             </div>
           </span>
         </Navbar.Collapse>

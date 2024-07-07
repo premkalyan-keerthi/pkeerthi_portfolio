63bf28e86964aabc4206041ff1eacf6b43ca5773
Prem Keerthi <pkeerthi@Prems-Air.lan>
2024-07-07 02:01:45 -0500

    added github link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon4 from '../assets/img/nav-icon4.svg';
 
 
 const NavBar =() => {
@@ -44,6 +45,7 @@ const NavBar =() => {
           <span className='navbar-text'>
             <div className='social-icon'> 
                 <a href="https://www.linkedin.com/in/pkeerthi52/" target= "_blank"> <img src={navIcon1}  alt=""/> </a>
+                <a href="https://github.com/premkalyan-keerthi" target= "_blank"> <img src={navIcon4}  alt=""/> </a>
                 <a href="https://www.facebook.com/premkalyan234" target= "_blank"> <img src={navIcon2}  alt=""/> </a>
                 <a href="https://www.instagram.com/premkalyan_k/" target= "_blank"> <img src={navIcon3}  alt=""/> </a>
             </div>

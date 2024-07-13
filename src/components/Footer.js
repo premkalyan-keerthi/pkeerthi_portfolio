ff12f382e7098c576a77d3b35e3b4544cd703a92
Prem Keerthi <pkeerthi@Prems-MacBook-Air.local>
2024-07-13 03:57:52 -0500

    latest changes for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ import contact from '../assets/img/contact-phone.svg';
 const Footer = () => {
   return (
     <footer className="footer" style={{backgroundColor: "white"}}>
+      <img src={location} alt="" /><p>location: Beaumont - Texas</p>
       <img src={mail} alt="" /><p>mail:premkalyan52@gmail.com</p>
-      <img src={location} alt="" /><p>contact: 4096001193</p>
-      <img src={contact} alt="" /><p>location: Beaumont - Texas</p>
+      <img src={contact} alt="" /><p>contact: 4096001193</p>
     </footer>
   )
 }
